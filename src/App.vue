cbe31ef1f9f504746c7a6038ee5cca82762eed47
l.yakubenko <l.yakubenko@rabota.ru>
2022-11-10 18:50:37 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Main from './components/Main.vue'
+</script>
+
+<template>
+  <div class="main">
+    <h1>Planner</h1>
+    <Main/>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  padding: 16px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(3)
+</script>
+
+<template>
+  <div class="card">
+    Today
+  </div>
+
+  <ul>
+    <li>first</li>
+    <li>second</li>
+    <li>third</li>
+  </ul>
+  <div>{{ count }} tasks</div>
+</template>
+
+<style scoped>
+.card {
+  color: #5a5a5a;
+}
+</style>
