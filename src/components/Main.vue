e95116b830de93bbb5be9c7e03d4be48dda29b0e
l.yakubenko <l.yakubenko@rabota.ru>
2022-11-11 20:54:55 +0300

    ant-design. modal and button init

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,47 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
 
-const count = ref(3)
+export default defineComponent({
+  setup() {
+    const visible = ref<boolean>(false);
+    const count = ref<number>(3)
+    const title = ref<string>('')
+    const description = ref<string>('')
+
+    const showModal = () => {
+      visible.value = true;
+    };
+
+    const handleOk = (e: MouseEvent) => {
+      visible.value = false;
+    };
+    return {
+      title,
+      description,
+      count,
+      visible,
+      showModal,
+      handleOk,
+    };
+  },
+});
 </script>
 
 <template>
   <div class="card">
-    Today
+    Сегодня
   </div>
 
   <ul>
     <li>first</li>
     <li>second</li>
     <li>third</li>
+    <a-button type="primary" @click="showModal">Добавить задачу</a-button>
+
+    <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk" ok-text="Добавить" cancel-text="Закрыть">
+      <a-input v-model:value="title" placeholder="Заголовок" class="card__modal-title-input"/>
+      <a-textarea :rows="4" v-model:value="description" placeholder="Описание" />
+    </a-modal>
   </ul>
   <div>{{ count }} tasks</div>
 </template>
@@ -20,5 +49,8 @@ const count = ref(3)
 <style scoped>
 .card {
   color: #5a5a5a;
+}
+.card__modal-title-input {
+  margin-bottom: 12px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { Button } from 'ant-design-vue';
+import { Input } from 'ant-design-vue';
+import { Modal } from 'ant-design-vue';
+
+import 'ant-design-vue/lib/button/style/index.css'
+import 'ant-design-vue/lib/modal/style/index.css'
+import 'ant-design-vue/lib/input/style/index.css'
+
+const app = createApp(App);
+
+app.use(Button)
+app.use(Modal)
+app.use(Input)
+
+app.mount('#app');
